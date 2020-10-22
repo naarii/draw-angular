9619db441d938e028ecb87b918e6926a36e157be
naarii <naarii94@gmail.com>
2020-10-22 23:37:39 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CanvasComponent } from './canvas/canvas.component';
+import { ToolSelectComponent } from './tool-select/tool-select.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    CanvasComponent,
+    ToolSelectComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/tool-select/tool-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-select',
+  templateUrl: './tool-select.component.html',
+  styleUrls: ['./tool-select.component.css']
+})
+export class ToolSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
